b637d7a55cb7f0cb3ba192e3caf5091b90db7a66
Diego vargas <diegoenrique.vr@gmail.com>
2025-02-24 11:19:40 -0600

    Substraction V1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ function calculate() {
     document.getElementById('result').innerText = "Result: " + result;
 }
 
+function subtract() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    let result;
+    result =  Number(num1) - Number(num2);
+
+    document.getElementById('result').innerText = "Result: " + result;
+}
+
 function resetFields() {
     document.getElementById('num1').value = "";
     document.getElementById('num2').value = "";
