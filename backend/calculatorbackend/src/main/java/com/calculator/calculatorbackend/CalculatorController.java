21be7c55f7d77dfc31784ad1fae2aa6fc4729ff4
Diego vargas <diegoenrique.vr@gmail.com>
2025-03-10 07:52:16 -0600

    Front API

--- backend/calculatorbackend/src/main/java/com/calculator/calculatorbackend/CalculatorController.java
+++ backend/calculatorbackend/src/main/java/com/calculator/calculatorbackend/CalculatorController.java
@@ -2,13 +2,16 @@ package com.calculator.calculatorbackend;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 public class CalculatorController {
     
+   
     @GetMapping("/add")
     public Calculator add(@RequestParam String num1, @RequestParam String num2) {
         int result = Integer.parseInt(num1) + Integer.parseInt(num2);
